4305efdaef00e0b7c8812b12f4a1c88be0d136b9
sebas1197 <eslandazuri@espe.edu.ec>
2023-04-24 20:44:45 -0500

    Paso 1

---
+++ main.py
@@ -0,0 +1,57 @@
+
+
+def opcion1():
+    print("Se ha seleccionado la opción 1.")
+
+def opcion2():
+    print("Se ha seleccionado la opción 2.")
+
+def opcion3():
+    print("Se ha seleccionado la opción 3.")
+
+def opcion4():
+    print("Se ha seleccionado la opción 4.")
+
+def opcion5():
+    print("Se ha seleccionado la opción 5.")
+
+def opcion6():
+    print("Se ha seleccionado la opción 6.")
+
+def menu():
+
+    print("Seleccione una opción:")
+    print("1. Mostrar vector y matriz de datos")
+    print("2. Ordenar vector")
+    print("3. Suma total vector")
+    print("4. Suma total matriz")
+    print("5. Primos del vector")
+    print("6. Impares de la matriz")
+    print("7. Ordenar matriz")
+    print("8. Salir")
+
+    opcion = input("Ingrese el número de opción: ")
+
+    if opcion == "1":
+        opcion1()
+    elif opcion == "2":
+        opcion2()
+    elif opcion == "3":
+        opcion3()
+    elif opcion == "4":
+        opcion4()
+    elif opcion == "5":
+        opcion5()
+    elif opcion == "6":
+        opcion6()
+    elif opcion == "7":
+        print("Has seleccionado la opción 7.")
+    elif opcion == "8":
+        print("Saliendo del programa...")
+        exit()
+    else:
+        print("Opción inválida. Por favor, seleccione una opción válida.")
+
+
+while True:
+    menu()
